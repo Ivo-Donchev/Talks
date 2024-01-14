53a4e6734c1d78f7ebdbd23b83c77e5aac4ff69c
Ivo Donchev <i.donchev@hacksoft.io>
2024-01-14 17:51:13 +0200

    Enable GZIP compressing

--- DevBg.Python.Meetup.February-2024/demo_project/django/demo/settings.py
+++ DevBg.Python.Meetup.February-2024/demo_project/django/demo/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
 ]
 
 ROOT_URLCONF = "demo.urls"
